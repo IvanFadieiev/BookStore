6b836394c9a3555c5c91fea3929ebf60eb373e64
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-09-12 11:30:35 +0300

    added changes according suggestions

--- src/main/java/project/bookstore/dto/BookRequestDto.java
+++ src/main/java/project/bookstore/dto/BookRequestDto.java
@@ -1,19 +1,19 @@
 package project.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class BookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
-    @Size(min = 13)
+    @ISBN
     private String isbn;
     @NotNull
     @Min(0)
