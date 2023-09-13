74de6d5fcaea3faf2957eae5bbcd9c12417b432c
Ivan Fadieiev <134369531+IvanFadieiev@users.noreply.github.com>
2023-09-13 13:43:32 +0300

    Update BookRequestDto.java

--- src/main/java/project/bookstore/dto/BookRequestDto.java
+++ src/main/java/project/bookstore/dto/BookRequestDto.java
@@ -3,6 +3,7 @@ package project.bookstore.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.validator.constraints.ISBN;
